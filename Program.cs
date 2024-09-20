c20a698db65664905c5a9a9f2e0df5d53ccd8ea9
Gustavo Pavan <gustavo.pavan@hotmail.com>
2024-09-20 11:49:50 -0300

    Solução trilha POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone Nokia = new Nokia ( numero: "0000000", modelo: "1", imei: "37812631287", memoria: 128);
+Nokia.Ligar();
+Nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Smartphone Apple = new Iphone ( numero: "11111111", modelo: "2", imei: "56235712", memoria: 256);
+Apple.ReceberLigacao();
+Apple.InstalarAplicativo("Instagram");
+
+
